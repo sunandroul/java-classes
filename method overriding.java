9151eb19787d50a8dd7762a433220e8db3f3afcf
sunandroul <sunandroul0@gmail.com>
2026-03-28 19:16:44 +0530

    Create method overriding.java

---
+++ method overriding.java
@@ -0,0 +1,19 @@
+class Animal {
+    void sound() {
+        System.out.println("Animal makes a sound");
+    }
+}
+
+class Dog extends Animal {
+    
+    void sound() {
+        System.out.println("Dog barks");
+    }
+}
+
+public class MethodOverridingDemo {
+    public static void main(String[] args) {
+        Animal obj = new Dog();  
+        obj.sound();             
+    }
+}
