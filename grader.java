e577ae37319dd1fd2335cf446c64da107ef4c140
sunandroul <sunandroul0@gmail.com>
2026-04-28 00:32:34 +0530

    grader.java

--- grader.java
+++ grader.java
@@ -19,3 +19,27 @@ public class Grader {
         }
     }
 }
+import java.util.Scanner;
+
+public class GraderDemo {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Please enter the student's score (0-100): ");
+        int inputScore = scanner.nextInt();
+
+        // Validate that the value is not negative and not greater than 100
+        if (inputScore < 0 || inputScore > 100) {
+            System.out.println("Invalid input! The score must be between 0 and 100.");
+        } else {
+            // Create Grader object
+            Grader studentGrader = new Grader(inputScore);
+            
+            // Get and print the grade
+            char grade = studentGrader.letterGrade();
+            System.out.println("The calculated letter grade is: " + grade);
+        }
+
+        scanner.close();
+    }
+}
